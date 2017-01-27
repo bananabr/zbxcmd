2d282ba372569d3c602d0aad2e9c3e087258b351
bananabr <bananabr@users.noreply.github.com>
2017-01-27 09:41:11 -0300

    removed unecessary filter

--- examples/add_host_dependencies.sh
+++ examples/add_host_dependencies.sh
@@ -10,7 +10,7 @@ CHILD_HOSTS="8.8.8.8"
 TRIGGER_PRIO=2
 
 PARENT_HOST_ID=`python zbxcmd.py -s $ZABBIX_BASE_URL -u $ZABBIX_USER -p $ZABBIX_PASSWORD -c host --method=get -F "host:$PARENT_HOST" -f hostid | jq ".hostid"`
-PARENT_TRIGGERS=`python zbxcmd.py -s $ZABBIX_BASE_URL -u $ZABBIX_USER -p $ZABBIX_PASSWORD -c host --method=get -F "host:$PARENT_HOST" -f hostid -f host -F "priority:2" --include-triggers | jq ".triggers[] | select((.priority | tonumber) > $TRIGGER_PRIO) | .triggerid" | sed 's/"//g'`
+PARENT_TRIGGERS=`python zbxcmd.py -s $ZABBIX_BASE_URL -u $ZABBIX_USER -p $ZABBIX_PASSWORD -c host --method=get -F "host:$PARENT_HOST" -f hostid -f host --include-triggers | jq ".triggers[] | select((.priority | tonumber) > $TRIGGER_PRIO) | .triggerid" | sed 's/"//g'`
 echo $PARENT_HOST_ID
 echo $PARENT_TRIGGERS
 for HOST in $CHILD_HOSTS
